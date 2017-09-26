3efb7beed7713335ba3e2a61ef0daa014b786e59
Ewelina Drozda <e.drozda@gmail.com>
2017-09-26 19:12:39 +0200

    poprawki w js

--- zad1/js/scripts.js
+++ zad1/js/scripts.js
@@ -1,20 +1,17 @@
 $(document).ready(function(){
 	var span = $("span");
-    span.each(function(index, element) {
-});
-span.each(function(index, element) {
-	if(index % 2 == 0) {
-		$(element).css('color', 'red');
-	};
-});
-var paragraphs = $('p');
-paragraphs.each(function(index, element) {
+	span.each(function(index, element) {
+		if(index % 2 == 0) {
+			$(element).css('color', 'red');
+		};
+	});
+	var paragraphs = $('p');
+	paragraphs.each(function(index, element) {
+		var button = '<button class="btn" data-tmp="' + index + '">Click this button</button>'
+    	$(element).append(button)
 
-    var button = '<button class="btn" data-tmp="' + index + '">Click this button</button>'
-    $(element).append(button)
-
-});
-$("button").click(function(){
-	alert($(this).attr("data-tmp"));
-});
+	});
+	$("button").click(function(){
+		alert($(this).attr("data-tmp"));
+	});
 });
