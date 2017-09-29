c6e763a1d0d25a808ca122bff30332521e11ac3e
Ewelina Drozda <e.drozda@gmail.com>
2017-09-29 11:05:21 +0200

    zdefiniowane funkcje changeSlide i moveFirstSlide

--- zad2/js/scripts.js
+++ zad2/js/scripts.js
@@ -1,11 +1,13 @@
 $(function(){
-	var carouselList = $("#carousel ul"),
-		interval = setInterval(function() {
-	carouselList.animate({'marginLeft':-300}, 3000, function() {
-		var firstItem = carouselList.find("li:first"),
-			lastItem = carouselList.find("li:last");
+	var carouselList = $("#carousel ul");
+		interval = setInterval(changeSlide, 3000);
+	function changeSlide() {
+		carouselList.animate({'marginLeft':-1500}, 8000, moveFirstSlide);
+	};
+	var firstItem = carouselList.find("li:first"),
+		lastItem = carouselList.find("li:last");
+	function moveFirstSlide() {
 		lastItem.after(firstItem);
 		carouselList.css({marginLeft:0});
-		});
-	});
+	};
 });
